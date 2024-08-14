91e45d4321cb6d78197506937f957fa2781aab83
Sanam Gurung <sanam.bdr.gurung@gmail.com>
2024-08-14 11:41:56 +0545

    shuffle the flag list, add IBAction and UIALertController inside Vm

--- guess-the-flag/ViewController.swift
+++ guess-the-flag/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet var button3: UIButton!
     var countries=[String]()
     var score = 0
-    
+    var correctAnswer = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         button1.layer.borderWidth = 1
@@ -24,13 +24,35 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-        askQuestion()
+        askQuestion(action: nil)
     }
     
-    func askQuestion(){
+   
+    func askQuestion(action: UIAlertAction?){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        title = countries[correctAnswer].uppercased()
     }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        }else{
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default,handler: askQuestion))
+        present(ac,animated: true)
+        
+    }
+    
 }
 
